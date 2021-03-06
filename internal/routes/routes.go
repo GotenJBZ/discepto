712f89b384c4cd93a88f10c88ff2441f3d3a8745
ranfdev <ranfdev@gmail.com>
2021-03-06 18:35:45 +0100

    Set cookies to HttpOnly

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -26,6 +26,9 @@ type Routes struct {
 
 func NewRouter(config *models.EnvConfig, db *db.DB, log zerolog.Logger, tmpls *render.Templates) chi.Router {
 	cookiestore := sessions.NewCookieStore(config.SessionKey)
+	cookiestore.Options = &sessions.Options {
+		HttpOnly: true,
+	}
 	routes := &Routes{envConfig: config, db: db, tmpls: tmpls, cookiestore: cookiestore}
 	r := chi.NewRouter()
 
